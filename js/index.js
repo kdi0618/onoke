67b204304e5f6d1640f30eade8a1e74e304a8306
hayate <hayate.224@icloud.com>
2021-06-16 23:03:07 +0900

    fix:HTMLの機能改善、JavaScriptの機能を追加しました

--- js/index.js
+++ js/index.js
@@ -5,5 +5,9 @@ console.log(titleElm);
 titleElm.classList.add('is-active');
 
 // ↓↓↓以下に実装してください
+document.querySelector('.keep').onclick=function(){
+    this.style.backgroundColor="#3fb811";
+
+};
 
 // ↑↑↑ここまで
